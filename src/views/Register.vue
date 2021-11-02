5f8a84a61131d17693c018cd2da25cfe83e1a02a
caiohscruz <caiohscruz@hotmail.com>
2021-11-02 18:59:45 -0300

    set error notification component

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -4,42 +4,16 @@
     <hr />
     <div class="columns is-centered">
       <div class="column is-half">
-        <div class="notification is-danger" v-show="msgError!=''">
-          <button class="delete" @click="closeNotification()"></button>
-          {{msgError}}
-        </div>
+        <ErrorNotification :msgError="msgError" />
         <label for="name">Usuário</label>
-        <input
-          type="text"
-          placeholder="Informe o nome de usuário"
-          id="name"
-          class="input"
-          v-model="name"
-        />
+        <input type="text" placeholder="Informe o nome de usuário" id="name" class="input" v-model="name" />
         <label for="email">E-mail</label>
-        <input
-          type="email"
-          placeholder="Informe seu melhor e-mail"
-          id="email"
-          class="input"
-          v-model="email"
-        />
+        <input type="email" placeholder="Informe seu melhor e-mail" id="email" class="input" v-model="email" />
         <label for="password">Senha</label>
-        <input
-          type="password"
-          placeholder="Informe sua senha"
-          id="password"
-          class="input"
-          v-model="password"
-        />
+        <input type="password" placeholder="Informe sua senha" id="password" class="input" v-model="password" />
         <label for="confirmpassword">Confirme a senha</label>
-        <input
-          type="password"
-          placeholder="Repita sua senha"
-          id="confirmpassword"
-          class="input"
-          v-model="confirmpassword"
-        />
+        <input type="password" placeholder="Repita sua senha" id="confirmpassword" class="input"
+          v-model="confirmpassword" />
         <br />
         <button class="button is-success" @click="register()">Cadastrar</button>
       </div>
@@ -48,43 +22,46 @@
 </template>
 
 <script>
-import axios from "axios";
+  import axios from "axios";
 
-export default {
-  data() {
-    return {
-      name: "",
-      password: "",
-      confirmpassword: "",
-      email: "",
-      msgError: ""
-    };
-  },
-  methods: {
-    register() {
-      if (this.password != this.confirmpassword) {
-        alert("Senhas não conferem");
-        return;
-      }
-      axios
-        .post("http://localhost:8686/user", {
-          name: this.name,
-          password: this.password,
-          email: this.email,
-        })
-        .then((res) => {
-          alert(res.data);
-          this.$router.push({name: 'Home'});
-        })
-        .catch((err) => {
-          this.msgError = err.response.data.err;
-        });
+  export default {
+    data() {
+      return {
+        name: "",
+        password: "",
+        confirmpassword: "",
+        email: "",
+        msgError: ""
+      };
     },
-    closeNotification(){
-        this.msgError = '';
+    methods: {
+      register() {
+        if (this.password != this.confirmpassword) {
+          this.msgError = "Senhas não conferem";
+          return;
+        }
+        axios
+          .post(`${process.env.VUE_APP_API}/user`, {
+            name: this.name,
+            password: this.password,
+            email: this.email,
+          })
+          .then((res) => {
+            alert(res.data);
+            this.$router.push({
+              name: 'Home'
+            });
+          })
+          .catch((err) => {
+            this.msgError = err.response.data;
+          });
+      },
+
+    },
+    components: {
+      ErrorNotification: () => import('../components/ErrorNotification.vue')
     }
-  },
-};
+  };
 </script>
 
 <style scoped>
