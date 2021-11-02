a274e2c9a29fc8ffaa51b315c6bc6a8a05fc72ff
caiohscruz <caiohscruz@hotmail.com>
2021-11-02 19:00:30 -0300

    set error notification component

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,64 +4,52 @@
     <hr />
     <div class="columns is-centered">
       <div class="column is-half">
-        <div class="notification is-danger" v-show="msgError!=''">
-          <button class="delete" @click="closeNotification()"></button>
-          {{msgError}}
-        </div>
+        <ErrorNotification :msgError="msgError" />
+
         <label for="email">E-mail</label>
-        <input
-          type="email"
-          placeholder="Informe seu melhor e-mail"
-          id="email"
-          class="input"
-          v-model="email"
-        />
+        <input type="email" placeholder="Informe seu melhor e-mail" id="email" class="input" v-model="email" />
         <label for="password">Senha</label>
-        <input
-          type="password"
-          placeholder="Informe sua senha"
-          id="password"
-          class="input"
-          v-model="password"
-        />
+        <input type="password" placeholder="Informe sua senha" id="password" class="input" v-model="password" />
         <br />
-        <button class="button is-success" @click="login()">Cadastrar</button>
+        <button class="button is-success" @click="login()">Entrar</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import axios from "axios";
+  import axios from "axios";
 
-export default {
-  data() {
-    return {
-      password: "",
-      email: "",
-      msgError: ""
-    };
-  },
-  methods: {
-    login() {
-      axios
-        .post("http://localhost:8686/login", {
-          password: this.password,
-          email: this.email,
-        })
-        .then((res) => {
-          alert(res.data);
-          this.$router.push({name: 'Home'});
-        })
-        .catch((err) => {
-          this.msgError = err.response.data;
-        });
+  export default {
+    data() {
+      return {
+        password: "",
+        email: "",
+        msgError: ""
+      };
     },
-    closeNotification(){
-        this.msgError = '';
+    methods: {
+      login() {
+        axios
+          .post(`${process.env.VUE_APP_API}/login`, {
+            password: this.password,
+            email: this.email,
+          })
+          .then((res) => {
+            localStorage.setItem('token', res.data);
+            this.$router.push({
+              name: 'Home'
+            });
+          })
+          .catch((err) => {
+            this.msgError = err.response.data;
+          });
+      },
+    },
+    components: {
+      ErrorNotification: () => import('../components/ErrorNotification.vue'),
     }
-  },
-};
+  };
 </script>
 
 <style scoped>
