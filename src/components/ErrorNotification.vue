c1a6eb1151a9b5967673d6a037b4db47eb512013
caiohscruz <caiohscruz@hotmail.com>
2021-11-02 18:57:26 -0300

    add error notification component

---
+++ src/components/ErrorNotification.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="notification is-danger" v-show="msgError!=''">
+        <button class="delete" @click="closeNotification()"></button>
+        {{msgError}}
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            msgError: String
+        },
+        methods: {
+            closeNotification() {
+                this.$parent.$data.msgError = '';
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
