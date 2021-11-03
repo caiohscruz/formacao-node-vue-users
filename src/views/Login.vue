5248c8f4cebbb6d987d801a1a0f86399db4a344b
caiohscruz1 <caiohscruz@hotmail.com>
2021-11-03 16:21:12 -0300

    add link to recover password

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -7,11 +7,27 @@
         <ErrorNotification :msgError="msgError" />
 
         <label for="email">E-mail</label>
-        <input type="email" placeholder="Informe seu melhor e-mail" id="email" class="input" v-model="email" />
+        <input
+          type="email"
+          placeholder="Informe seu melhor e-mail"
+          id="email"
+          class="input"
+          v-model="email"
+        />
         <label for="password">Senha</label>
-        <input type="password" placeholder="Informe sua senha" id="password" class="input" v-model="password" />
+        <input
+          type="password"
+          placeholder="Informe sua senha"
+          id="password"
+          class="input"
+          v-model="password"
+        />
         <br />
-        <p>Se ainda não possui cadastro, <router-link to="/register">clique aqui</router-link></p>
+        <p>
+          Se ainda não possui cadastro,
+          <router-link to="/register">clique aqui</router-link>
+        </p>
+        <p><a @click="recoverPassword()">Esqueci minha senha</a></p>
         <button class="button is-success" @click="login()">Entrar</button>
       </div>
     </div>
@@ -19,41 +35,57 @@
 </template>
 
 <script>
-  import axios from "axios";
+import axios from "axios";
 
-  export default {
-    data() {
-      return {
-        password: "",
-        email: "",
-        msgError: ""
-      };
+export default {
+  data() {
+    return {
+      password: "",
+      email: "",
+      msgError: "",
+    };
+  },
+  methods: {
+    login() {
+      axios
+        .post(`${process.env.VUE_APP_API}/login`, {
+          password: this.password,
+          email: this.email,
+        })
+        .then((res) => {
+          localStorage.setItem("token", res.data);
+          this.$router.push(
+            {
+              name: "RestrictArea",
+            },
+            {}
+          );
+        })
+        .catch((err) => {
+          this.msgError = err.response.data;
+        });
     },
-    methods: {
-      login() {
-        axios
-          .post(`${process.env.VUE_APP_API}/login`, {
-            password: this.password,
-            email: this.email,
-          })
-          .then((res) => {
-            localStorage.setItem('token', res.data);
-            this.$router.push({
-              name: 'RestrictArea'
-            },{});
-          })
-          .catch((err) => {
-            this.msgError = err.response.data;
-          });
-      },
+    recoverPassword() {
+      if (this.email == "") {
+        this.msgError = "Favor, informe um e-mail válido";
+      } else {
+        axios.post(`${process.env.VUE_APP_API}/recoverpassword`, {
+          email: this.email,
+        }).then(res =>{
+          alert(res.data);
+        }).catch(err=>{
+          this.msgError = err.response.data;
+        })
+      }
     },
-    components: {
-      ErrorNotification: () => import('../components/ErrorNotification.vue'),
-    },
-    created(){
+  },
+  components: {
+    ErrorNotification: () => import("../components/ErrorNotification.vue"),
+  },
+  created() {
     this.$parent.$data.scope = "out";
-  }
-  };
+  },
+};
 </script>
 
 <style scoped>
