a2cffd94b596407a49c72359f9680a8b89f4915c
caiohscruz <caiohscruz@hotmail.com>
2021-11-05 18:35:22 -0300

    create navbar components

---
+++ src/components/NavBarInternal.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="nav">
+      <router-link to="/restrict-area">Área Restrita</router-link> |
+      <router-link to="/admin/users">Gerenciamento de Usuários</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "NavBarIntternal"
+
+}
+</script>
+
+<style>
+
+</style>
